3c79f35224bf6f81afcd4e74a543287c6ee24d21
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-06-07 22:03:01 +0600

    Frontend Project Pushed

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/LogIn";
+import SignIn from "./pages/Auth/SignIn";
+import Home from "./pages/Home/Home";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1> */}
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Home />}/>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
