c476c396f70fa71896bf7fd03c83e00d375bae9a
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-06-14 00:07:43 +0600

    Auth Issues Solved

--- src/components/cards/ProfileInfo.tsx
+++ src/components/cards/ProfileInfo.tsx
@@ -1,6 +1,6 @@
-// import { getInitials } from "../../utils/helper";
+import { getInitials } from "../../utils/helper";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { logout } from "../../features/auth/auththunks";
 
 const ProfileInfo = () => {
@@ -8,8 +8,9 @@ const ProfileInfo = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
 
+    const user = useAppSelector((state) => state.auth.user);
+
     const onLogOut = () => {
-        // localStorage.clear();
         dispatch(logout());
         navigate("/login");
     };
@@ -17,11 +18,10 @@ const ProfileInfo = () => {
     return (
         <div className="flex items-center gap-3">
             <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-               {/* {getInitials()}  */}
-               WJ
+                {getInitials(user  ? user?.fullName : "")}
             </div>
             <div>
-                <p className="text-sm font-medium">Dustin</p>
+                <p className="text-sm font-medium">{user?.fullName || ""}</p>
                 <button className="text-sm text-slate-700 underline hover:cursor-pointer" onClick={onLogOut}>Log Out</button>
             </div>
         </div>
--- src/features/auth/auththunks.ts
+++ src/features/auth/auththunks.ts
@@ -2,6 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../api/axiosInstance";
 import type { AuthResponse,User } from "./authTypes";
 
+export const signUp = createAsyncThunk<AuthResponse, { fullName: string, email: string; password: string }>('auth/signup', async (credentials, { rejectWithValue}) => {
+    try{
+        const response = await axiosInstance.post<AuthResponse>('/auth/sign-up', credentials);
+        return response.data;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }catch(err: any){
+        return rejectWithValue(err.response?.data?.message || 'Login failed');
+    }
+});
+
 export const login = createAsyncThunk<AuthResponse, { email: string; password: string }>('auth/login', async (credentials, { rejectWithValue}) => {
     try{
         const response = await axiosInstance.post<AuthResponse>('/auth/log-in', credentials);
